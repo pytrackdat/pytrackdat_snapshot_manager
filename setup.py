5601dda86166c1f09d3942547201e6bfe2e3686a
David Lougheed <david.lougheed@gmail.com>
2019-12-08 10:23:47 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pytrackdat_snapshot_manager",
+    version="0.1.0",
+
+    python_requires="~=3.6",
+    install_requires=["Django>=2.2.8,<3.0"],
+
+    description='Snapshot manager Django app for PyTrackDat sites.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/pytrackdat/pytrackdat_snapshot_manager",
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"  # Maybe should be refined
+    ],
+
+    author="David Lougheed",
+    author_email="david.lougheed@gmail.com",
+
+    packages=["pytrackdat_snapshot_manager"],
+    include_package_data=True
+)
